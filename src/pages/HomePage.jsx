7eba7f2ce5bee8a808a9131f1b7e282cdde43a81
marinn999 <marinacompel@icloud.com>
2025-02-18 09:00:41 +0100

    beg

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h2 className="text-4xl text-center">Home</h2>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+  return (
+    <div>
+      <img src="https://images.app.goo.gl/YY3NHCLH4F7Kq1hv8" width={1000} />
+      <h2 className="font-bold text-7xl text-center">Page not found</h2>
+    </div>
+  );
+};
